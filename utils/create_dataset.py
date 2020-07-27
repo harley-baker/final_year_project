b53ab03f2fe226445a6c6b3937ed63d0b02d1d73
Harley <harley.baker@students.plymouth.ac.uk>
2020-07-27 17:42:59 +0100

    refactor data parsing to extract JSON reading logic

--- utils/create_dataset.py
+++ utils/create_dataset.py
@@ -18,69 +18,74 @@ def parse(input_tsv, input_json, output_file):
                     account_id = int(line[0])
                     account_class = line[1]
                     for account in data:
-                        if user_exists:
-                            user = account['user']
-                            created_at = account['created_at']
-                            user_id = user['id']
-                            user_age = datetime.now(timezone.utc) - datetime.strptime(created_at,
-                                                                                      '%a %b %d %H:%M:%S %z %Y')
-                        else:
-                            user = account
-                            created_at = account['user_created_at']
-                            user_id = user['user_id']
-                            user_age = datetime.now() - datetime.strptime(created_at, '%a %b %d %H:%M:%S %Y')
-                        if user_id == account_id:
-                            # Get feature data
-
-                            # Metadata features
-                            status_count = user['statuses_count']
-                            follower_count = user['followers_count']
-                            friends_count = user['friends_count']
-                            favourites_count = user['favourites_count']
-                            listed_count = user['listed_count']
-                            default_profile = user['default_profile']
-                            profile_use_background_image = user['profile_use_background_image']
-                            verified = user['verified']
-                            screen_name = user['screen_name']
-                            name = user['name']
-                            description = user['description']
-
-                            # Derived features
-                            tweet_freq = user['statuses_count'] / user_age.seconds
-                            follower_growth_rate = follower_count / user_age.seconds
-                            friend_growth_rate = friends_count / user_age.seconds
-                            favourites_growth_rate = favourites_count / user_age.seconds
-                            listed_growth_rate = favourites_count / user_age.seconds
-                            followers_friends_ratio = follower_count / friends_count if friends_count > 0 else 0
-                            screen_name_length = len(screen_name)
-                            num_digits_in_screen_name = sum(c.isdigit() for c in screen_name)
-                            name_length = len(name)
-                            num_digits_in_name = sum(c.isdigit() for c in name)
-                            description_length = len(description) if description else 0
-
-                            # Write to file
-                            out_writer.writerow([
-                                float(account_id),
-                                status_count,
-                                follower_count,
-                                friends_count,
-                                favourites_count,
-                                listed_count,
-                                default_profile,
-                                profile_use_background_image,
-                                verified,
-                                float(tweet_freq),
-                                float(follower_growth_rate),
-                                float(friend_growth_rate),
-                                float(favourites_growth_rate),
-                                float(listed_growth_rate),
-                                float(followers_friends_ratio),
-                                screen_name_length,
-                                num_digits_in_screen_name,
-                                name_length,
-                                num_digits_in_name,
-                                description_length,
-                                account_class
-                            ])
+                        # Write to file
+                        data = parse_json_to_data(account, user_exists, account_id)
+                        out_writer.writerow([
+                            float(account_id),
+                            data['status_count'],
+                            data['follower_count'],
+                            data['friends_count'],
+                            data['favourites_count'],
+                            data['listed_count'],
+                            data['default_profile'],
+                            data['profile_use_background_image'],
+                            data['verified'],
+                            float(data['tweet_freq']),
+                            float(data['follower_growth_rate']),
+                            float(data['friend_growth_rate']),
+                            float(data['favourites_growth_rate']),
+                            float(data['listed_growth_rate']),
+                            float(data['followers_friends_ratio']),
+                            data['screen_name_length'],
+                            data['num_digits_in_screen_name'],
+                            data['name_length'],
+                            data['num_digits_in_name'],
+                            data['description_length'],
+                            account_class
+                        ])
 
     print('Done with %s' % input_tsv)
+
+
+def parse_json_to_data(json_obj, user_exists, account_id=None):
+    if user_exists:
+        user = json_obj['user']
+        created_at = json_obj['created_at']
+        user_id = user['id']
+        user_age = datetime.now(timezone.utc) - datetime.strptime(created_at,
+                                                                  '%a %b %d %H:%M:%S %z %Y')
+    else:
+        user = json_obj
+        created_at = json_obj['user_created_at']
+        user_id = user['user_id']
+        user_age = datetime.now() - datetime.strptime(created_at, '%a %b %d %H:%M:%S %Y')
+    if user_id == account_id or account_id is None:
+        # Get feature data
+        data = {}
+        # Metadata features
+        data['status_count'] = user['statuses_count']
+        data['follower_count'] = user['followers_count']
+        data['friends_count'] = user['friends_count']
+        data['favourites_count'] = user['favourites_count']
+        data['listed_count'] = user['listed_count']
+        data['default_profile'] = user['default_profile']
+        data['profile_use_background_image'] = user['profile_use_background_image']
+        data['verified'] = user['verified']
+        data['screen_name'] = user['screen_name']
+        data['name'] = user['name']
+        data['description'] = user['description']
+
+        # Derived features
+        data['tweet_freq'] = user['statuses_count'] / user_age.seconds
+        data['follower_growth_rate'] = data['follower_count'] / user_age.seconds
+        data['friend_growth_rate'] = data['friends_count'] / user_age.seconds
+        data['favourites_growth_rate'] = data['favourites_count'] / user_age.seconds
+        data['listed_growth_rate'] = data['favourites_count'] / user_age.seconds
+        data['followers_friends_ratio'] = data['follower_count'] / data['friends_count'] if\
+            data['friends_count'] > 0 else 0
+        data['screen_name_length'] = len(data['screen_name'])
+        data['num_digits_in_screen_name'] = sum(c.isdigit() for c in data['screen_name'])
+        data['name_length'] = len(data['name'])
+        data['num_digits_in_name'] = sum(c.isdigit() for c in data['name'])
+        data['description_length'] = len(data['description']) if data['description'] else 0
+        return data
