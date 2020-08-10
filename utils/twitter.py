a128c9f9b8e218860de583348127e8488f8b5b51
Harley <harley.baker@students.plymouth.ac.uk>
2020-08-10 14:09:45 +0100

    remove api keys

---
+++ utils/twitter.py
@@ -0,0 +1,18 @@
+import tweepy
+import json
+
+with open('keys.json') as f:
+    data = json.load(f)
+
+CONSUMER_KEY = data['CONSUMER_KEY']
+CONSUMER_SECRET = data['CONSUMER_SECRET']
+ACCESS_TOKEN = data['ACCESS_TOKEN']
+ACCESS_SECRET = data['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
+
+def get_user(user):
+    return api.get_user(user)
